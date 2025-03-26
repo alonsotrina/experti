77f8bf5239847f46102c89c360255518b791b7aa
Alonso <alonso@MacBook-Air-de-Alonso.local>
2025-03-26 00:26:16 -0300

    nuevo diseño v-01

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',  // Asegúrate de agregar el archivo HTML principal
+    './src/**/*.{html,js}',  // Si tienes una estructura de carpetas con archivos JS o HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
